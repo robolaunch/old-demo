e399c0841be14cf53458c6eb67baff421e5c768e
esgungor <engin.gungor14@gmail.com>
2021-12-03 11:30:52 +0100

    kubeclient function added

---
+++ launch/pkg/kubeclient.go
@@ -0,0 +1,19 @@
+package kubeclient
+
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func GetKubeClient() (*kubernetes.Clientset, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", "./config.yaml")
+	if err != nil {
+		return nil, err
+	}
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return client, err
+
+}
