0065602dd1a0ea0605c3663cad71437f240a4ac6
esgungor <engin.gungor14@gmail.com>
2021-12-07 14:31:22 +0100

    #8 mongodb initialization and create/delete methods implemented

--- launch/go.mod
+++ launch/go.mod
@@ -13,22 +13,32 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logr/logr v0.4.0 // indirect
 	github.com/go-resty/resty/v2 v2.6.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang-jwt/jwt/v4 v4.1.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/googleapis/gnostic v0.5.5 // indirect
 	github.com/imdario/mergo v0.3.5 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/segmentio/ksuid v1.0.4 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.0.0-20210928044308-7d9f5e0b762b // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9 // indirect
 	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
 	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
 	golang.org/x/text v0.3.6 // indirect
--- launch/server/launch.go
+++ launch/server/launch.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	launchpb "github.com/robolaunch/demo-launch/launch/api/launch"
 	"github.com/robolaunch/demo-launch/launch/pkg/account"
 	"github.com/robolaunch/demo-launch/launch/pkg/kubeclient"
+	"github.com/robolaunch/demo-launch/launch/pkg/persistance"
 	"google.golang.org/grpc/metadata"
 )
 
@@ -13,24 +15,40 @@ import (
 //TODO: Implement helm chart for it.
 func (*server) CreateLaunch(ctx context.Context, req *launchpb.LaunchCreateRequest) (*launchpb.LaunchResponse, error) {
 	// Username could be used for namespace in this version.
-	headers, _ := metadata.FromIncomingContext(ctx)
 
-	// Check user is here!
-	_ = account.CurrentUser(headers["authorization"][0])
+	// User check service implemented user cannot access without login!
+	headers, _ := metadata.FromIncomingContext(ctx)
+	err := account.CurrentUser(headers["authorization"][0])
+	if err != nil {
+		return nil, err
+	}
 
 	username := req.GetLaunch().GetUsername()
+
 	// namespace := req.GetLaunch().GetNamespace()
 
 	//Launch Name
 	name := req.GetLaunch().GetName()
 
-	//TODO: Check user is available before creation
-
-	//Predefined deployment and docker templates.
+	// Predefined deployment and docker templates.
 	// Not implemented yet.
 	// robotType := req.GetLaunch().GetRobotType()
-	err := kubeclient.CreateDeploymentService(name, username)
+
+	err = kubeclient.CreateDeploymentService(name, username)
 	if err != nil {
+		return nil, err
+	}
+
+	// Save user instance into MongoDB
+	err = persistance.SaveLaunch(&persistance.Launch{
+		Username:  username,
+		Name:      name,
+		Namespace: username,
+		Type:      "DemoType",
+		Status:    true,
+	})
+	if err != nil {
+		fmt.Printf("User couldn't saved: %v", err)
 		return nil, err
 	}
 	return &launchpb.LaunchResponse{
@@ -52,6 +70,11 @@ func (*server) DeleteLaunch(ctx context.Context, req *launchpb.LaunchDeleteReque
 	//Predefined deployment and docker templates.
 	// Not implemented yet.
 	// robotType := req.GetLaunch().GetRobotType()
+	persistance.DeleteLaunch(&persistance.Launch{
+		Username:  username,
+		Name:      name,
+		Namespace: username,
+	})
 	err := kubeclient.DeleteDeploymentService(name, username)
 	if err != nil {
 		return nil, err
